57108bcc32bbda67a507933cf0b62421835bee61
Thomas <thomas_dru@hotmail.com>
2023-03-16 15:23:11 -0400

    styles, bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title')</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-         <link rel="stylesheet" href="/css/styles.css">
-         <script src="js/scripts.js"></script>
-    </head>
-    <body class="antialiased">
+    <title>@yield('title')</title>
+    <!-- Fonte do Google -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
-        @yield('content')
+    <!-- CSS Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-        <footer>
-            <p>Jhow Bananas &copy; 2023</p>
-        </footer>
-    </body>
+    <!-- CSS da aplicação -->
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="/js/scripts.js"></script>
+</head>
+
+<body class="antialiased">
+
+    @yield('content')
+
+    <footer>
+        <p>Jhow Bananas &copy; 2023</p>
+    </footer>
+</body>
+
 </html>
