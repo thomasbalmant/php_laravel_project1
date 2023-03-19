1a314838b95ea2c13d8d4ed1440d4233d9bba977
Thomas <thomas_dru@hotmail.com>
2023-03-19 16:01:35 -0400

    MAIN

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,7 +28,7 @@
                 <a href="/" class="nav-link">Eventos</a>
               </li>
               <li class="nav-item">
-                <a href="/" class="nav-link">Criar Eventos</a>
+                <a href="/events/create" class="nav-link">Criar Eventos</a>
               </li>
               <li class="nav-item">
                 <a href="/" class="nav-link">Entrar</a>
@@ -42,7 +42,8 @@
       </header>
       @yield('content')
       <footer>
-        <p>HDC Events &copy; 2020</p>
+        <p>JHOW BANANAS &copy; 2023</p>
       </footer>
+      <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
     </body>
 </html>
